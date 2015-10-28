b945a6acf3d920dd8a3e9ff07cfaaae17eb94c6f
John M Buscemi <jmbuscemi@gmail.com>
2015-10-28 10:25:56 -0400

    Add .append in JS

--- graph.js
+++ graph.js
@@ -1,11 +1,14 @@
 var scale = d3.scale.linear()
     .domain([2000, 2012])
     .range([50, 950]);
+
 var axis = d3.svg.axis()
     .scale(scale)
     .orient("bottom")
     .ticks(13)
     .tickFormat(d3.format("d"));
+    
 var g = d3.select("g");
 
 g.call(axis);
+g.append("circle").attr("cx", scale(2005)).attr("r", 5)
