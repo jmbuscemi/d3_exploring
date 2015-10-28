2aadaf39871776c5bbef713ded49b69b9f82ba90
John M Buscemi <jmbuscemi@gmail.com>
2015-10-28 10:20:31 -0400

    Remove shapes, add axis (with ticks) for a graph

--- graph.js
+++ graph.js
@@ -1,1 +1,11 @@
+var scale = d3.scale.linear()
+    .domain([2000, 2012])
+    .range([50, 950]);
+var axis = d3.svg.axis()
+    .scale(scale)
+    .orient("bottom")
+    .ticks(13)
+    .tickFormat(d3.format("d"));
+var g = d3.select("g");
 
+g.call(axis);
