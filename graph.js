4b1e453e9faecc16787500b69c0f5f805481e2fd
John M Buscemi <jmbuscemi@gmail.com>
2015-10-28 11:05:21 -0400

    Create loop with d3.csv function to display all data

--- graph.js
+++ graph.js
@@ -3,7 +3,7 @@ g = svg.append("g");
 g.attr("transform", "translate(100,50)");
 
 x = d3.scale.linear()
-    .domain([2000, 2009])  // Fill in the domain values for the x axis
+    .domain([1997, 2015])  // Fill in the domain values for the x axis
     .range([0, 800]);
 y = d3.scale.linear()
     .domain([0, 60])  // Fill in the domain values for the y axis
@@ -18,16 +18,25 @@ gx = g.append("g")
 gx.call(x_axis);
 gx.attr("transform", "translate(0,400)");
 
+d3.csv("old_discoveries.csv", function(data) {
+  data.forEach(function(point){
+    g.append("circle")
+      .attr("cx", x(point.year))
+      .attr("cy", y(point.important_discoveries))
+      .attr("r", point.important_discoveries);
+  });
+});
+
 // Okay, now all of your axes are set up.  Add code to draw points here.
 //Fill in the parens and add stuff after the last dot, then make more of these lines.
-g.append("circle").attr("cx", x(2000)).attr("cy", y(45)).attr("r", 45);
-g.append("circle").attr("cx", x(2001)).attr("cy", y(4)).attr("r", 4);
-g.append("circle").attr("cx", x(2002)).attr("cy", y(2)).attr("r", 2);
-g.append("circle").attr("cx", x(2003)).attr("cy", y(2)).attr("r", 2);
-g.append("circle").attr("cx", x(2004)).attr("cy", y(4)).attr("r", 4);
-g.append("circle").attr("cx", x(2005)).attr("cy", y(12)).attr("r", 12);
-g.append("circle").attr("cx", x(2006)).attr("cy", y(23)).attr("r", 23);
-g.append("circle").attr("cx", x(2007)).attr("cy", y(54)).attr("r", 54);
-g.append("circle").attr("cx", x(2008)).attr("cy", y(12)).attr("r", 12);
-g.append("circle").attr("cx", x(2009)).attr("cy", y(1)).attr("r", 1) ;
-;
+// g.append("circle").attr("cx", x(2000)).attr("cy", y(45)).attr("r", 45);
+// g.append("circle").attr("cx", x(2001)).attr("cy", y(4)).attr("r", 4);
+// g.append("circle").attr("cx", x(2002)).attr("cy", y(2)).attr("r", 2);
+// g.append("circle").attr("cx", x(2003)).attr("cy", y(2)).attr("r", 2);
+// g.append("circle").attr("cx", x(2004)).attr("cy", y(4)).attr("r", 4);
+// g.append("circle").attr("cx", x(2005)).attr("cy", y(12)).attr("r", 12);
+// g.append("circle").attr("cx", x(2006)).attr("cy", y(23)).attr("r", 23);
+// g.append("circle").attr("cx", x(2007)).attr("cy", y(54)).attr("r", 54);
+// g.append("circle").attr("cx", x(2008)).attr("cy", y(12)).attr("r", 12);
+// g.append("circle").attr("cx", x(2009)).attr("cy", y(1)).attr("r", 1) ;
+// ;
